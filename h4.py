402310492eab7d9bbf591ae89ec9109850d805ed
zikun <zikun.rain@gmail.com>
2019-05-19 19:51:52 -0700

    h4

--- h4.py
+++ h4.py
@@ -2,71 +2,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 EPS = 1e-6
-MAXITER = 100
+MAXITER = 1000
 
-def gauss(pars, x, v0 = 0, isNoise = False):
-    a, b, c, offset = pars[:4]
+def gauss(parameters, x, isNoise = False):
+    a, b, c, offset = parameters
     n = np.random.normal(size=len(x), scale=0.05) if isNoise else np.zeros((len(x),))
-    return a * np.exp(- (x - b) ** 2 / (2 * c ** 2)) + offset - v0 + n
+    return a * np.exp(- (x - b) ** 2 / (2 * c ** 2)) + offset + n
 
 
-def gauss_jacobian(pars, x, v0 = 0):
-    a, b, c, offset = pars[:4]
+def gaussJacobian(parameters, x, v):
+    a, b, c, offset = parameters
     f = a * np.exp(- (x - b) ** 2 / (2 * c ** 2))
     J = np.zeros(shape=(4,) + x.shape)
     J[0] = 1. / a * f
     J[1] = f * (x - b) / c ** 2
     J[2] = f * (x - b) ** 2 / c ** 3
     J[3] = 1
-    return f + (offset - v0), J
+    return f + (offset - v), J
 
 
-def LM(fn, pars, args, tau = 1e-4):
-    p, I = pars, np.eye(len(pars))
+def LM(fn, parameters, args, tau = 1e-4):
+    p = parameters
+    I = np.eye(len(parameters))
     f, J = fn(p, *args)
-    A, g,  = np.inner(J, J), np.inner(J, f)
-    k, nu, mu = 0, 2, tau * np.max(np.diag(A))
-    stop = np.linalg.norm(g, np.Inf) < EPS
+    JJ = np.inner(J, J)
+    JF = np.inner(J, f)
+    k, nu = 0, 2
+    lam = tau * np.max(np.diag(JJ))
+    stop = np.linalg.norm(JF, np.Inf) < EPS
 
     while not stop and k < MAXITER:
-
         k += 1
-        d = np.linalg.solve(A + mu * I, -g)
-        if np.linalg.norm(d) < EPS * (np.linalg.norm(p) + EPS):
+        pDelta = np.linalg.solve(JJ + lam * I, -JF)
+        if np.linalg.norm(pDelta) < EPS * (np.linalg.norm(p) + EPS):
             break
-        pnew = p + d
+        pnew = p + pDelta
         fnew, Jnew = fn(pnew, *args)
-        rho = (np.linalg.norm(f) ** 2 - np.linalg.norm(fnew) ** 2) / np.inner(d, mu * d - g)
+        rho = (np.linalg.norm(f) ** 2 - np.linalg.norm(fnew) ** 2) / np.inner(pDelta, lam * pDelta - JF)
         if rho > 0:
-            p, A, g, f, J = pnew, np.inner(Jnew, Jnew), np.inner(Jnew, fnew), fnew, Jnew
-            if np.linalg.norm(g, np.Inf) < EPS:
+            # update
+            p = pnew
+            f = fnew
+            J = Jnew
+            JJ = np.inner(Jnew, Jnew)
+            JF = np.inner(Jnew, fnew)
+            # update end
+
+            if np.linalg.norm(JF, np.Inf) < EPS:
                 break
-            mu = mu * np.max([1. / 3, 1. - (2. * rho - 1.) ** 3])
+            lam = lam * np.max([1. / 3, 1. - (2. * rho - 1.) ** 3])
             nu = 2.
         else:
-            mu = mu * nu
+            lam = lam * nu
             nu = 2 * nu
-        print('Iter %d: |f|=%f, mu=%f, rho=%f' % (k, np.linalg.norm(f), mu, rho))
+        print('\nIter %d: |f|=%f, lam=%f, rho=%f' % (k, np.linalg.norm(f), lam, rho))
+        print('updated p %s' % str(pnew.tolist()))
     else:
         print('max iteration reached')
     return p
 
 
 def main():
-    pars = [1, 0.1, 1, 0.5]
-    ipars = [2, 0.5, 2, 0.5]
-    x = np.linspace(-3, 3, 300)
+    parameters, initialParameters = [1, 0.1, 1, 0.5], [4, 1, 2, 0.3]
+    x = np.linspace(-4, 4, 200)
+    fig = plt.figure()
 
-    yv = gauss(pars, x)
-    plt.plot(x, yv, 'g')
+    yTrue = gauss(parameters, x)
+    green_ele, = plt.plot(x, yTrue, 'g')
 
-    y = gauss(pars, x, isNoise=True)
-    plt.plot(x, y, 'bx')
+    yNoise = gauss(parameters, x, isNoise=True)
+    blue_ele, = plt.plot(x, yNoise, 'bx')
 
-    pn = LM(gauss_jacobian, ipars, (x, y))
-    print(pn)
-    yn = gauss(pn, x)
-    plt.plot(x, yn, 'r')
+    pn = LM(gaussJacobian, initialParameters, (x, yNoise))
+    yFIt = gauss(pn, x)
+    red_ele, = plt.plot(x, yFIt, 'r')
+
+    fig.legend((green_ele, blue_ele, red_ele), ('gauss', 'sample', 'fit'), 'upper right')
+
     plt.show()
 
 if __name__ == '__main__':
