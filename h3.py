486cde03926edcdfc2adab896c1467ab520e6401
zikun <zikun.rain@gmail.com>
2019-05-20 06:21:56 -0700

    h3

--- h3.py
+++ h3.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import operator
+import copy
 
 def generate_2d_gaussian(N, meanX, meanY, var):
     x = np.random.normal(loc=meanX, scale=var, size=N)
@@ -9,9 +10,9 @@ def generate_2d_gaussian(N, meanX, meanY, var):
     return x, y
 
 def generate_data():
-    x0, y0 = generate_2d_gaussian(300, 0, 0, 2)
-    x1, y1 = generate_2d_gaussian(200, 7, 2, 1.5)
-    x2, y2 = generate_2d_gaussian(200, 3, 5, 2)
+    x0, y0 = generate_2d_gaussian(300, 0, 0, 1.5)
+    x1, y1 = generate_2d_gaussian(200, 7, 2, 1)
+    x2, y2 = generate_2d_gaussian(200, 3, 5, 1.5)
     data = np.dstack((np.concatenate((x0, x1, x2)), np.concatenate((y0, y1, y2))))[0]
     return data
 
@@ -74,11 +75,12 @@ def gmm(pts, K):
         Lprev = L
     return np.argmax(prob, axis=1)
 
-class pt():
-    def __init__(self, d, N):
-        self.x = d[0]
-        self.y = d[1]
+class Pt():
+    def __init__(self, id, d, N):
+        self.d = np.array(d)
         self.prob = [0 for i in range(N)]
+        self.id = id
+        self.children = []
     
     def addProb(self, round):
         self.prob[round] += 1
@@ -87,30 +89,74 @@ class pt():
         index, value = max(enumerate(self.prob), key=operator.itemgetter(1))
         return index, value
 
+    def getDistance(self, pt):
+        return np.linalg.norm(self.d - pt.d)
+    
+    def shiftSearch(self, id, pts, bandWidth=2):
+        shift = np.array([0., 0.])
+        nNeighbor = 0
+        for pt in pts:
+            dist = self.getDistance(pt)
+            if dist < bandWidth:
+                pt.prob[id] += 1
+                shift += (pt.d - self.d)
+                nNeighbor += 1
+        return shift / nNeighbor
+    
+    def update(self, shift):
+        self.d = self.d + shift
+
+
 def generate_pts(data):
     pts = []
-    for d in data:
-        pts.append(pt(d))
+    for id, d in enumerate(data):
+        pts.append(Pt(id, d, len(data)))
     return pts
-        
 
-# def mean_shift(pts, K):
-#     N = len(pts)
-#     initialStartPoint = 
+def mergeDetect(pt, centers, r=3):
+    for index, c in enumerate(centers):
+        if c.getDistance(pt) < r:
+            return True, index
+    return False, -1    
+
+def mean_shift(pts, bandWidth=2, eps=1e-6):
+    centers = []
+    
+    for initialPt in pts:
+        pt = copy.deepcopy(initialPt)
+        while True:
+            shift = pt.shiftSearch(pt.id, pts)
+            pt.update(shift)
+            if np.linalg.norm(shift) < eps:
+                flag, index = mergeDetect(pt, centers)
+                if flag:
+                    centers[index].children.append(pt.id)
+                else:
+                    centers.append(pt)
+                break
+    mapping = {}
+    for ci, c in enumerate(centers):
+        mapping[c.id] = ci
+        for childId in c.children:
+            mapping[childId] = ci
+    cluster = []
+    for pt in pts:
+        cluster.append(mapping[pt.getCluster()[0]])
+    return cluster
 
 
 def main():
-    Clusters = ['rx', 'gx', 'bx']
+    cluster_markers = ['rx', 'gx', 'bx', 'kx', 'kx', 'kx', 'kx', 'kx']
     data = generate_data()
     pts = generate_pts(data)
-    
+    cluster = mean_shift(pts)
 
 
     # cr = gmm(np.dstack((x, y))[0], len(Clusters))
-    # for i, c in enumerate(cr):
-    #     plt.plot(x[i], y[i], Clusters[c])
-    # #plt.plot(x, y, 'rx')
-    # plt.show()
+    for i, d in enumerate(data):
+        plt.plot(d[0], d[1], cluster_markers[cluster[i]])
+    #plt.plot(x, y, 'rx')
+    plt.show()
 
 if __name__ == '__main__':
     main()
